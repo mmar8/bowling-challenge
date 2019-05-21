0472f34fde7db94ab92a4d74c11198f0fb028c7e
Maria Vanaale <mvanaale@gmail.com>
2019-05-21 16:22:06 +0100

    score is 20 if rolls all ones

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -14,4 +14,11 @@ describe("Bowling Game", function() {
     }
     expect(game.score()).toBe(0);
   });
+
+  it("should return 20 if all ones", function() {
+    for (var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toBe(20);
+  });
 });
