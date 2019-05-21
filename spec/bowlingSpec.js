277118d15e1268cfc43a2124863ee3909a7fb426
Maria Vanaale <mvanaale@gmail.com>
2019-05-21 16:49:00 +0100

    refactored code

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -8,17 +8,20 @@ describe("Bowling Game", function() {
     game = new BowlingGame();
   });
 
-  it("should be able to return 0 if gutter game", function() {
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+  it("can return 0 if gutter game", function() {
+    rollMany(0, 20)
     expect(game.score()).toBe(0);
   });
 
-  it("should return 20 if all ones", function() {
-    for (var i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+  it("can return 20 if all ones", function() {
+    rollMany(1, 20);
     expect(game.score()).toBe(20);
   });
+
+    var rollMany = function(pins, rolls) {
+      for (var i = 0; i < rolls; i++){
+        game.roll(pins);
+      }
+    }
+
 });
